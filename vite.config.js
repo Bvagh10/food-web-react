304068dd264a9cc65f1a6f886b262919d971fa32
Babu Vagh <bvagh011@gmail.com>
2024-12-06 00:31:41 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  base: '/food-web-react/', // Replace 'my-project' with your repository name
 });
+
